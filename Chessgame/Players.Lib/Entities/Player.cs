54279f7eea36847c2240350a4aa099225ccde107
Giel-Jan Verstraete <contact@gieljanverstraete.be>
2019-05-22 20:50:02 +0200

    Add constructor to Player.cs

--- Chessgame/Players.Lib/Entities/Player.cs
+++ Chessgame/Players.Lib/Entities/Player.cs
@@ -10,5 +10,11 @@ namespace Players.Lib.Entities
     {
         public string Name { get; set; }
         public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
     }
 }

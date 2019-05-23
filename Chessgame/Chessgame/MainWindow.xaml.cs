9da7fa190401a55778bed1f01b32e44b5bdf9c5c
Giel-Jan Verstraete <contact@gieljanverstraete.be>
2019-05-23 17:53:27 +0200

    Add reference and using statements to main window

--- Chessgame/Chessgame/MainWindow.xaml.cs
+++ Chessgame/Chessgame/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Players.Lib.Entities;
+using Players.Lib.Services;
 
 namespace Chessgame
 {

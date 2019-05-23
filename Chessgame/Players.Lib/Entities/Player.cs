b19c307e21444458279f8f2122e8ef08b706daef
Giel-Jan Verstraete <contact@gieljanverstraete.be>
2019-05-23 20:18:57 +0200

    Add list of pawns to player

--- Chessgame/Players.Lib/Entities/Player.cs
+++ Chessgame/Players.Lib/Entities/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pawns.Lib.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,14 @@ namespace Players.Lib.Entities
         public string Name { get; set; }
         public int Score { get; set; }
         public int Index { get; set; }
+        public List<Pawn> PlayerPawns { get; set; }
 
         public Player(string name, int index, int score)
         {
             Name = name;
             Score = score;
             Index = index;
+            PlayerPawns = new List<Pawn>();
         }
     }
 }
---
+++ Chessgame/Players.Lib/Services/PawnService.cs
@@ -0,0 +1,19 @@
+﻿using Pawns.Lib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pawns.Lib.Services
+{
+    public class PawnsService
+    {
+        public List<Pawn> Pawns { get; set; }
+
+        public PawnsService()
+        {
+            Pawns = new List<Pawn>();
+        }
+    }
+}
